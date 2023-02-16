1fa97bb28d9c28b618bee0a8b10a408810273aef
Miles Granger <miles59923@gmail.com>
2023-02-16 08:40:52 +0100

    compatible with context in callback platform functions

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::ffi::c_void;
+
 #[allow(unused_imports)]
 use defmt::*;
-
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Flex, Input, Level, Output, Pull, Speed};
+#[allow(unused_imports)]
+use embassy_stm32::{
+    gpio::{Flex, Input, Level, Output, Pin, Pull, Speed},
+    peripherals::PB14,
+};
 use embassy_time::{block_for, Duration, Instant, Timer};
+use spin::Mutex;
 use stm_tof::{ffi::VL53LX_DEV, Platform, Result, VL53L4CX_Device};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -18,18 +24,25 @@ static WARN_CM: f32 = 90.; // When yellow light turns on, green if greater
 async fn main(_spawner: Spawner) {
     let peripherals = embassy_stm32::init(Default::default());
 
-    let mut led_green = Output::new(peripherals.PB14, Level::High, Speed::Low);
-    let mut platform = Platform::new();
-    platform.set_read_byte(|| crate::unimplemented!("read_byte not implemented"));
-    platform.set_wait_millis(|n| block_for(Duration::from_millis(n as _)));
+    let mut led_green = Mutex::new(Output::new(peripherals.PB14, Level::High, Speed::Low));
+
+    let ctx = &mut led_green as *mut _ as *mut c_void;
+
+    let mut platform = Platform::new(ctx);
+    platform.set_read_byte(|ctx| crate::unimplemented!("read_byte not implemented"));
+    platform.set_wait_millis(|ctx, n| {
+        let led_green = unsafe { &mut *(ctx.0 as *mut Mutex<Output<PB14>>) };
+        led_green.lock().set_high();
+        block_for(Duration::from_millis(n as _));
+    });
 
     let mut distance_sensor = VL53L4CX_Device::new(0x11, platform);
 
     distance_sensor.init().unwrap();
     loop {
-        led_green.set_high();
+        led_green.lock().set_high();
         Timer::after(Duration::from_millis(250)).await;
-        led_green.set_low();
+        led_green.lock().set_low();
         Timer::after(Duration::from_millis(250)).await;
     }
 }
