a5c3f08a3731e082954cbd60e04d74a6620133a9
Miles Granger <miles59923@gmail.com>
2023-12-31 13:35:21 +0100

    Chkpt - updated a121 lib maybe just generate bindings and go from there?

--- build.rs
+++ build.rs
@@ -7,28 +7,40 @@
 //! Cargo re-run the build script whenever `memory.x` is changed,
 //! updating `memory.x` ensures a rebuild of the application with the
 //! new memory settings.
-
-use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
+use std::fs;
 
 fn main() {
-    // Put `memory.x` in our output directory and ensure it's
-    // on the linker search path.
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
+    // // Put `memory.x` in our output directory and ensure it's
+    // // on the linker search path.
+    // let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    // File::create(out.join("memory.x"))
+    //     .unwrap()
+    //     .write_all(include_bytes!("memory.x"))
+    //     .unwrap();
+    // println!("cargo:rustc-link-search={}", out.display());
+
+    // // By default, Cargo will re-run a build script whenever
+    // // any file in the project changes. By specifying `memory.x`
+    // // here, we ensure the build script is only re-run when
+    // // `memory.x` is changed.
+    // println!("cargo:rerun-if-changed=memory.x");
+    let files: Vec<std::path::PathBuf> = fs::read_dir("vendored/xm125/Src")
         .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out.display());
+        .filter(|f| f.as_ref().unwrap().path().ends_with(".c"))
+        .map(|f| f.unwrap().path())
+        .collect();
 
-    // By default, Cargo will re-run a build script whenever
-    // any file in the project changes. By specifying `memory.x`
-    // here, we ensure the build script is only re-run when
-    // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x");
+    cc::Build::new()
+        .file("vendored/xm125/Src/examples/getting_started/example_bring_up.c")
+        .file("vendored/xm125/Src/integration/acc_hal_integration_stm32cube_xm.c")
+        .include("vendored/xm125/Inc")
+        .compile("xm125");
 
+    println!("cargo:rustc-link-lib=acconeer_a121");
+    println!("cargo:rustc-link-lib=acc_detector_distance_a121");
+    println!(
+        "cargo:rustc-link-search=/home/milesg/Projects/garage-backup-sensor/vendored/xm125/lib"
+    );
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
--- src/main.rs
+++ src/main.rs
@@ -2,125 +2,32 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-#[allow(unused_imports)]
-use defmt::*;
+use core::ffi::c_char;
 
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Flex, Input, Level, Output, Pull, Speed};
-use embassy_time::{block_for, Duration, Instant, Timer};
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-static STOP_CM: f32 = 40.; // When red light turns on
-static WARN_CM: f32 = 90.; // When yellow light turns on, green if greater
+extern "C" {
+    // fn acc_hal_rss_integration_get_implementation();
+    fn acconeer_main(argc: i32, argv: *mut c_char) -> i32;
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let peripherals = embassy_stm32::init(Default::default());
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World, calling sensor count!");
+    let count = unsafe { acconeer_main(0, core::ptr::null_mut() as _) };
+    info!("Sensor count: {}", count);
 
-    let mut led_red = Output::new(peripherals.PA2, Level::High, Speed::Low);
-    let mut led_yellow = Output::new(peripherals.PB0, Level::High, Speed::Low);
-    let mut led_green = Output::new(peripherals.PC0, Level::High, Speed::Low);
-    let button = Input::new(peripherals.PC13, Pull::None);
-
-    let mut laser = Flex::new(peripherals.PA3);
-
-    Timer::after(Duration::from_millis(100)).await;
-
-    led_green.set_low();
-    led_yellow.set_low();
-    led_red.set_low();
-
-    let interval = Duration::from_millis(250);
-    let mut distance_prev = 0f32;
-    let mut last_significant_change = Instant::now();
-    let mut stop_cm = STOP_CM;
-    let mut warn_cm = WARN_CM;
+    let mut led = Output::new(p.PB14, Level::High, Speed::Low);
 
     loop {
-        // Query laser for measurement, ref pg 3 of datasheet.
-        // 1. Ensure set low, followed by high pulse of 2micros (min), the low.
-        // 2. Wait for laser pulse, then measure the width.
-        laser.set_as_output(Speed::Medium);
-        laser.set_low();
-        block_for(Duration::from_micros(5));
-
-        laser.set_high();
-        block_for(Duration::from_micros(10));
-        laser.set_low();
-
-        laser.set_as_input(Pull::None);
-        while laser.is_low() {}
-
-        let inst = Instant::now();
-        while laser.is_high() {}
-        let duration = inst.elapsed();
-
-        // Invalid/Inacurate measurements: Ref pg 3 of datasheet.
-        // We need an valid measurement.
-        if duration.as_millis() > 12 {
-            led_green.set_low();
-            led_yellow.set_low();
-            led_red.set_low();
-            Timer::after(Duration::from_secs(1)).await;
-            continue;
-        }
-
-        // Ref Ping Laser datasheet pg. 4
-        let distance_curr = (duration.as_micros() as f32 * 171.5) / 10000.;
-
-        // Check if we're setting the distance
-        let button_pressed_start = Instant::now();
-        while button.is_high() {
-            if button_pressed_start.elapsed() >= Duration::from_secs(5) {
-                stop_cm = distance_curr;
-                warn_cm = stop_cm + 50.;
-                for _ in 0..3 {
-                    led_green.set_high();
-                    led_yellow.set_high();
-                    led_red.set_high();
-                    Timer::after(Duration::from_millis(250)).await;
-                    led_green.set_low();
-                    led_yellow.set_low();
-                    led_red.set_low();
-                    Timer::after(Duration::from_millis(250)).await;
-                }
-                break;
-            }
-            Timer::after(Duration::from_millis(250)).await;
-        }
-
-        // Determine if distance is actively changing since last, more than 20cm
-        let mut active = true;
-        if num::abs(distance_prev - distance_curr) < 20. {
-            // Last change over 30s ago, turn off all lights.
-            if last_significant_change.elapsed().as_secs() > 30 {
-                led_green.set_low();
-                led_yellow.set_low();
-                led_red.set_low();
-                // Extra sleep until next check since nothing is happening.
-                Timer::after(interval).await;
-                active = false;
-            }
-        } else {
-            last_significant_change = Instant::now();
-        };
-
-        if active {
-            if distance_curr <= stop_cm {
-                led_green.set_low();
-                led_yellow.set_low();
-                led_red.set_high();
-            } else if distance_curr <= warn_cm {
-                led_green.set_low();
-                led_yellow.set_high();
-                led_red.set_low();
-            } else {
-                led_green.set_high();
-                led_yellow.set_low();
-                led_red.set_low();
-            }
-            distance_prev = distance_curr;
-        }
-        Timer::after(interval).await;
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
     }
 }
